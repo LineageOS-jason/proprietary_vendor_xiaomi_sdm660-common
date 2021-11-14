d773c0e708f805093691480c1baed4392fe9d79f
Edwin Moquete <edwinmmoquete@gmail.com>
2021-11-14 19:08:14 +0000

    sdm660-common: Update Soter blobs from S62Pro
    Change-Id: I2af194cdfb76ee94aeb4af327651dc8d229dc51f
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- sdm660-common-vendor.mk
+++ sdm660-common-vendor.mk
@@ -478,7 +478,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm660-common/proprietary/vendor/radio/qcril_database/upgrade/7_version_update_ecc_table.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/7_version_update_ecc_table.sql
 
 PRODUCT_PACKAGES += \
-    SoterService \
     CneApp \
     IWlanService \
     TimeService \
